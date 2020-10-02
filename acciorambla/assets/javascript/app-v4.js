b751228b4d9b78dd13ce120c7ed4ccfcb485045b
annakallo <anna.kallo@gmail.com>
2020-10-02 23:42:32 +0300

    Acciorambla.

--- acciorambla/assets/javascript/app-v4.js
+++ acciorambla/assets/javascript/app-v4.js
@@ -136,8 +136,6 @@ window.addEventListener('load', function () {
         autoHeight: true,
         loop: true,
         nav: false,
-        lazyLoad: true,
-        lazyLoadEager: 1
     });
 
     // Go to the next item
